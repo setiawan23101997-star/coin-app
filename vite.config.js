048b0e611468e210205d375dd69260a111e6ebd8
Ryuji <setiawan23101997@gmail.com>
2026-09-04 03:56:37 +0700

    Fix MIME types for Netlify

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,15 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
+  },
+  server: {
+    port: 5173
   }
 })

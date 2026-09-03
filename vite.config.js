24f63603c94c70cea4aea02f381328a78f39e04d
Ryuji <setiawan23101997@gmail.com>
2026-09-04 04:10:29 +0700

    Fix MIME types and use single file build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    assetsInlineLimit: 100000000,
+    cssCodeSplit: false,
     rollupOptions: {
       output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
-  }
+        inlineDynamicImports: true,
+      },
+    },
+  },
 })

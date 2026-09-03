0d3b109c1f838f2f3f53b708a42db57510c636b3
Ryuji <setiawan23101997@gmail.com>
2026-09-04 04:18:42 +0700

    Fix MIME types and use inline assets

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 export default defineConfig({
-  plugins: [react(), viteSingleFile()],
+  plugins: [react()],
   build: {
     outDir: 'dist',
     assetsInlineLimit: 100000000,
-    cssCodeSplit: false,
     rollupOptions: {
       output: {
         inlineDynamicImports: true,
